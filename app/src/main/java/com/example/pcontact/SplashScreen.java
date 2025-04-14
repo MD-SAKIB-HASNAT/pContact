0db2a90591017bacf6cf1aa47fd564e22feac85d
MD-SAKIB-HASNAT <mdsakibkhan29@gmail.com>
2025-04-14 17:38:57 +0600

    sp add

--- app/src/main/java/com/example/pcontact/SplashScreen.java
+++ app/src/main/java/com/example/pcontact/SplashScreen.java
@@ -1,6 +1,8 @@
 package com.example.pcontact;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +17,11 @@ public class SplashScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash_screen);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(SplashScreen.this, NewContact.class));
+            finish();
+        }, 2000); // 2-second splash
+
     }
 }
